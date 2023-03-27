0f9902c839bca51ab4639830fe77dacbbb150912
Roberto Franco <robertofranco8090@gmail.com>
2023-03-27 12:15:43 +0200

    added dashboard page

---
+++ frontend/src/components/DashboardPost.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { FaPen, FaPencilAlt, FaTimes } from 'react-icons/fa';
+import styles from '@/styles/DashboardPost.module.css';
+
+export default function DashboardPost({ post, handleDelete }) {
+  return (
+    <div className={styles.post}>
+      <h4>
+        <Link href={`/posts/${post.attributes.slug}`}>
+          {post.attributes.title}
+        </Link>
+      </h4>
+
+      <Link href={`/posts/edit/${post.id}`}>
+        <div className={styles.edit}>
+          <FaPencilAlt /> <span>Edit Post</span>
+        </div>
+      </Link>
+      <button className={styles.delete} onClick={() => handleDelete(post.id)}>
+        <FaTimes /> <span>Delete</span>
+      </button>
+    </div>
+  );
+}
--- frontend/src/pages/account/dashboard.js
+++ frontend/src/pages/account/dashboard.js
@@ -1,9 +1,40 @@
+import { parseCookies } from '@/helpers/index';
 import Layout from '@/components/layout';
+import DashboardPost from '@/components/DashboardPost';
+import { API_URL } from '@/config';
+import styles from '@/styles/Dashboard.module.css';
 
-export default function DashboardPage() {
+export default function DashboardPage({ posts }) {
+  const deletePost = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Posts</h3>
+
+        {posts.map((post) => (
+          <DashboardPost key={post.id} post={post} handleDelete={deletePost} />
+        ))}
+      </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/api/posts/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const posts = await res.json();
+
+  return {
+    props: { posts: posts.data },
+  };
 }
