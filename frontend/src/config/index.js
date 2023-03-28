6ffacf14c1a25dd78006f56315e43cb0ad79a7df
Roberto Franco <robertofranco8090@gmail.com>
2023-03-28 08:49:38 +0200

    modified next url

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -4,4 +4,4 @@ export const API_URL =
     : 'http://127.0.0.1:1337';
 
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
